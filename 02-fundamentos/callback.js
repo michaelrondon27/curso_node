d591cbca10f209c034b079ed65afbc7fc821ffcb
mrondon <mrondon-externo@cnmc.es>
2019-11-14 12:49:36 +0100

    19. Callbacks

---
+++ 02-fundamentos/callback.js
@@ -0,0 +1,27 @@
+// setTimeout(() => {
+//     console.log('Hola Mundo');
+// }, 3000);
+
+let getUsuarioById = (id, callback) => {
+
+    let usuario = {
+        nombre: 'Michael',
+        id
+    }
+
+    if (id == 20) {
+        callback(`El usuario con id ${ id }, no existe en la BD`);
+    } else {
+        callback(null, usuario);
+    }
+
+}
+
+getUsuarioById(10, (err, usuario) => {
+
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log('Usuario de base de datos', usuario);
+});
