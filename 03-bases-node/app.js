37d973798c48e2ab05c358ac295f98562f80e297
mrondon <mrondon-externo@cnmc.es>
2019-11-15 11:00:40 +0100

    30. Importar archivos de nuestro proyecto

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,15 +1,5 @@
-// Requireds
-const fs = require('fs');
+const { crearArchivo } = require('./multiplicar/multiplicar');
 
-let base = 5;
+let base = '5';
 
-let data = '';
-
-for (let i = 1; i <= 10; i++) {
-    data += `${ base } * ${ i } = ${ base * i }\n`;
-}
-
-fs.writeFile(`tablas/tabla-${ base }.txt`, data, (err) => {
-    if (err) throw err;
-    console.log(`El archivo tabla-${ base }.txt ha sido creado`);
-});
+crearArchivo(base).then(archivo => console.log(`Archivo creado: ${ archivo }`)).catch(e => console.log(e));
