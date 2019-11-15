778ce42dccee82716780ad92e451e432a1b515a3
mrondon <mrondon-externo@cnmc.es>
2019-11-15 10:47:19 +0100

    29. Requerir paquetes - required

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,5 +1,15 @@
-let base = 3;
+// Requireds
+const fs = require('fs');
+
+let base = 5;
+
+let data = '';
 
 for (let i = 1; i <= 10; i++) {
-    console.log(`${ base } * ${ i } = ${ base * i }`);
+    data += `${ base } * ${ i } = ${ base * i }\n`;
 }
+
+fs.writeFile(`tablas/tabla-${ base }.txt`, data, (err) => {
+    if (err) throw err;
+    console.log(`El archivo tabla-${ base }.txt ha sido creado`);
+});
