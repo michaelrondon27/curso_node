3ff15eea124b9a73cef89a8b60d22b5c45c260c9
mrondon <mrondon-externo@cnmc.es>
2019-11-15 13:09:53 +0100

    36. Colores de la consola

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,4 +1,5 @@
 const argv = require('./config/yargs').argv;
+const colors = require('colors');
 const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
 let comando = argv._[0];
@@ -10,7 +11,7 @@ switch (comando) {
         break;
 
     case 'crear':
-        crearArchivo(argv.base, argv.limite).then(archivo => console.log(`Archivo creado: ${ archivo }`)).catch(e => console.log(e));
+        crearArchivo(argv.base, argv.limite).then(archivo => console.log(`Archivo creado: ${ archivo.green }`)).catch(e => console.log(e));
         break;
 
     default:
