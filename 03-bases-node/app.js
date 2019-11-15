5c28053e78017880b013dd1908917f0fb2045ddc
mrondon <mrondon-externo@cnmc.es>
2019-11-15 11:09:58 +0100

    31. Recibir informacion desde linea de comandos

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,5 +1,7 @@
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
-let base = '5';
+let argv = process.argv;
+let parametro = argv[2];
+let base = parametro.split('=')[1];
 
 crearArchivo(base).then(archivo => console.log(`Archivo creado: ${ archivo }`)).catch(e => console.log(e));
