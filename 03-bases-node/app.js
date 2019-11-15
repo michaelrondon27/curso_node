843941f875abd67988872e7935b267d773e5bf99
mrondon <mrondon-externo@cnmc.es>
2019-11-15 11:33:48 +0100

    33. Yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,7 +1,26 @@
+const argv = require('yargs')
+    .command(
+        'listar',
+        'Imprime en consola la tabla de multiplicar', {
+            base: {
+                demand: true,
+                alias: 'b'
+            },
+            limite: {
+                aliad: 'l',
+                default: 10
+            }
+        })
+    .help()
+    .argv;
+
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1];
+let argv2 = process.argv;
 
-crearArchivo(base).then(archivo => console.log(`Archivo creado: ${ archivo }`)).catch(e => console.log(e));
+console.log(argv.limite);
+
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
+
+// crearArchivo(base).then(archivo => console.log(`Archivo creado: ${ archivo }`)).catch(e => console.log(e));
