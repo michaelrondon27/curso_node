b162082b163a7b23f03819b5e193946752a38a14
mrondon <mrondon-externo@cnmc.es>
2019-11-20 17:18:40 +0100

    56. Peticiones HTTP - Axios - Request

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -6,4 +8,18 @@ const argv = require('yargs').options({
     }
 }).argv;
 
-console.log(argv);
+const endoedUrl = encodeURI(argv.direccion);
+
+const instance = axios.create({
+    baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ endoedUrl }`,
+    headers: {
+        'x-rapidapi-host': 'devru-latitude-longitude-find-v1.p.rapidapi.com',
+        'x-rapidapi-key': 'e475694fb0mshc28a0fb8c4e2575p155d44jsnad564a28da19'
+    }
+});
+
+instance.get().then(resp => {
+    console.log(resp.data.Results[0]);
+}).catch(err => {
+    console.log('ERROR!!!!', err);
+});
