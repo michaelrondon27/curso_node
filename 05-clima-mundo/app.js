56aa32aafd62bc2d2875b46c4a2f9085e45448bf
mrondon <mrondon-externo@cnmc.es>
2019-11-21 16:20:26 +0100

    59. Conectando ambos servicios

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -9,6 +9,22 @@ const argv = require('yargs').options({
     }
 }).argv;
 
-// lugar.getLugarLatLng(argv.direccion).then(console.log);
+const getInfo = async(direccion) => {
 
-clima.getClima(40.419998, -3.700000).then(console.log).catch(console.log);
+    try {
+
+        const coords = await lugar.getLugarLatLng(direccion);
+
+        const temp = await clima.getClima(coords.lat, coords.lng);
+
+        return `El clima de ${ coords.direccion } es de ${ temp }`;
+
+    } catch (e) {
+
+        return `No se pudo determinar el clima de ${ direccion }`;
+
+    }
+
+}
+
+getInfo(argv.direccion).then(console.log).catch(console.log);
