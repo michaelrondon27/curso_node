bcc6148e394f9dd47cb32e3ee0ca6973226d21c6
mrondon <mrondon-externo@cnmc.es>
2019-11-25 16:22:38 +0100

    66. Servir contenido estatico

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-    // res.send('Hello World')
+app.use(express.static(__dirname + '/public'));
 
-    let salida = {
-        nombre: 'Michael',
-        edad: 26,
-        url: req.url
-    };
+// app.get('/', function(req, res) {
+//     // res.send('Hello World')
 
-    res.send(salida);
-});
+//     let salida = {
+//         nombre: 'Michael',
+//         edad: 26,
+//         url: req.url
+//     };
+
+//     res.send(salida);
+// });
 
 app.listen(8080, () => {
     console.log('Escuchando peticiones en el puerto 8080')
