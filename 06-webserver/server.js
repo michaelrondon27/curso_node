3dc406fb7a12cee17b9f07f5030be636c7832577
mrondon <mrondon-externo@cnmc.es>
2019-11-25 16:57:25 +0100

    69. Usando parciales con HBS

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
 app.use(express.static(__dirname + '/public'));
 
 // Expres HBS Engine
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.get('/', function(req, res) {
     res.render('home', {
         nombre: 'Michael',
+        anio: new Date().getFullYear()
+    });
+});
+
+app.get('/about', function(req, res) {
+    res.render('about', {
         anio: new Date().getFullYear()
     });
 });
